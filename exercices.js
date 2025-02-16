3f7c6f67a95eacc9262d97ce9aef60715146d5d7
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-02-16 12:18:17 +0100

    Update instructions

--- exercices.js
+++ exercices.js
@@ -11,12 +11,6 @@ const formRechercheArtefact = document.querySelector(
   ".form__recherche_artefact"
 );
 
-const epoques = {
-  romaine: "Romaine",
-  medievale: "Médievale",
-  jurassique: "Jurassique",
-};
-
 const creerLesChoixEpoque = (epoques) => {
   const selectHtml = formChoixEpoqueHtml.querySelector("select");
   Object.entries(epoques).forEach(([id_epoque, nom_epoque]) => {
@@ -31,10 +25,20 @@ function generationNombreAleatoireEntre(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Permet d'afficher l'époque de destination du voyage
 const afficherDestination = (nomEpoque) =>
   (localisationEpoqueHTML.textContent = nomEpoque);
 
+// Permet d'afficher un artefact trouvée, ou non, à une époque
+const afficherRechercheArtefact = ({ artefact, epoque, success = true }) => {
+  const li = document.createElement("li");
+  li.textContent = `${success ? "✅" : "❌"} ${artefact} (Epoque ${epoque})`;
+  listeArtefactHTML.appendChild(li);
+};
+
 // Execution
+
+// Gestion envoi formulaire choix époque
 formChoixEpoqueHtml.addEventListener("submit", (event) => {
   event.preventDefault();
   const epoque = new FormData(formChoixEpoqueHtml).get("epoque");
@@ -47,33 +51,45 @@ formChoixEpoqueHtml.addEventListener("submit", (event) => {
   quandEpoqueChoisie(epoque);
 });
 
+// Gestion envoi formulaire recherche artelefact
 formRechercheArtefact.addEventListener("submit", (event) => {
   event.preventDefault();
   const artefact = new FormData(formRechercheArtefact).get("artefact");
   quandRechercheArtefact(artefact);
 });
 
-const afficherRechercheArtefact = ({ artefact, epoque, success = true }) => {
-  const li = document.createElement("li");
-  li.textContent = `${success ? "✅" : "❌"} ${artefact} (Epoque ${epoque})`;
-  listeArtefactHTML.appendChild(li);
-};
-
 /**
  * Votre partie commence ici, la partie modifiable par vos soins
  */
-let nomEpoqueActuelle;
+function main() {
+  // Sera modifié par le dernier exercice
+  const epoques = {
+    romaine: "Romaine",
+    medievale: "Médievale",
+    jurassique: "Jurassique",
+  };
 
-creerLesChoixEpoque(epoques);
+  // Permet d'être réutilisé dans la fonction quandRechercheArtefact
+  let nomEpoqueActuelle;
 
-// Fonction appelée plus haut quand le formulaire de voyage temporel est soumis
-// et qu'une époque de destination du voyage temporel a été choisi
-function quandEpoqueChoisie(nomEpoque) {
-  nomEpoqueActuelle = nomEpoque;
-  // Utilisation de votre fonction voyagerTemps
+  // Création dynamique des époques de destination de la machine temporelle
+  creerLesChoixEpoque(epoques);
+
+  // Fonction appelée plus haut quand le formulaire de voyage temporel est envoyé
+  // et qu'une époque de destination du voyage temporel a été choisi
+  // Faites le test depuis la page HTML
+  function quandEpoqueChoisie(nomEpoque) {
+    nomEpoqueActuelle = nomEpoque;
+    console.log(nomEpoqueActuelle);
+    // Utilisation de votre fonction voyagerTemps
+  }
+
+  // Fonction appelée plus haut quand le formulaire de recherche d'artefact est envoyé
+  // Faites le test depuis la page HTML
+  function quandRechercheArtefact(artefact) {
+    console.log(artefact);
+    // Utilisation de votre fonction collecterArtefact
+  }
 }
 
-// Fonction appelée plus haut quand le formulaire de recherche d'artefact est soumis
-function quandRechercheArtefact(artefact) {
-  // Utilisation de votre fonction collecterArtefact
-}
+main();
